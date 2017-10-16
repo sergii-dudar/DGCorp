2006101175a607e6ea634fb34eedb2cd84d4be3a
serhii.dudar <shtanga.net@gmail.com>
2017-10-16 20:39:56 +0300

    some update

--- Sources/DGCorp.Web/Controllers/SampleDataController.cs
+++ Sources/DGCorp.Web/Controllers/SampleDataController.cs
@@ -31,10 +31,7 @@ namespace DGCorp.Web.Controllers
             public int TemperatureC { get; set; }
             public string Summary { get; set; }
 
-            public int TemperatureF
-            {
-                get { return 32 + (int) (TemperatureC / 0.5556); }
-            }
+            public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
         }
     }
 }
