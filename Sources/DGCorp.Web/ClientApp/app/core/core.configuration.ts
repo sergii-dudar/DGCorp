c92163f4e00b8d1eb7aeb46815568e3fbeeea6cf
Sergii Dudar <sdudar@VIW00264>
2017-10-19 16:44:16 +0300

    update

--- Sources/DGCorp.Web/ClientApp/app/core/core.configuration.ts
+++ Sources/DGCorp.Web/ClientApp/app/core/core.configuration.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class CoreConfiguration {
-    public Server = 'http://localhost:93/api';
+    public Server = 'http://localhost:5000/api';
 
     public getActionUri(controller: string, action?: string) {
         return action ? `${this.Server}/${controller}/${action}` : `${this.Server}/${controller}`;
